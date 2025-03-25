f4bb3bd7b481a4854c3c6f03ce6c6805d9bb9e2c
K1rtz <djidicdavid@proton.me>
2025-03-25 01:27:18 +0100

    message routes and message controller added

--- back-end/src/routes/message.route.ts
+++ back-end/src/routes/message.route.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import protectRoute from '../middleware/protectRoute.js';
+import { getMessages, getUsersForSidebar, sendMessage } from '../controllers/messageController.js';
 
 const router = express.Router();
 
-router.get('/conversations', (req, res) =>{
-    res.send("Conversation route");
-})
+
+//Ne moze svako da salje poruke samo ulogovani korisnici pa imamo protectRoute
+router.get("/conversations", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
+
+
 
 export default router
