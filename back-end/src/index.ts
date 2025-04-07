da7aa5e93ed2fc4c0ffe2413e35eef26018e0e77
K1rtz <djidicdavid@proton.me>
2025-04-07 12:48:59 +0200

    fixing problem with routes

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -13,22 +13,22 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 // const app = express()
 
-
-
-
 app.use(cookieParser());
 app.use(express.json());
- 
+
+//moved up
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+
+
+
+
 if(process.env.NODE_ENV !== "development"){
     app.use(express.static(path.join(__dirname, "/front-end/dist")));
     app.get("*",(req, res)=>{
         res.sendFile(path.join(__dirname, "front-end", "dist", "index.html"));
-    })
-}
-
-
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
+    })    
+}    
 
 server.listen(PORT, () => {
     console.log("Server is running on port " + PORT )
